d2b9312de6b02c02eaaeae4829c47f08d8a0761c
Enzo Haegel <enzo.haegel@epitech.eu>
2022-05-05 22:44:17 +0200

    add interface and mapper for profil

---
+++ WebSite/src/app/pages/player-profil/player-profil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-profil',
+  templateUrl: './player-profil.component.html',
+  styleUrls: ['./player-profil.component.scss']
+})
+export class PlayerProfilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
